9149861554b3f3bb1f050e624a5d9278796440f3
Chelsea Davis <chelseadavis@hotmail.com>
2016-02-18 14:13:11 -0800

    Title Screen complete!
    Everything is positioned correctly :)

--- shmup_altered/src/shmup/HomeScene.hx
+++ shmup_altered/src/shmup/HomeScene.hx
@@ -22,15 +22,25 @@ class HomeScene
         var background = new FillSprite(0x03A3B3, System.stage.width, System.stage.height);
         scene.addChild(new Entity().add(background));
 
+        var title = new ImageSprite(ctx.pack.getTexture("jelly/title"));
+        title.x._ = System.stage.width/18;
+        title.y._ = System.stage.height/15;
+        scene.addChild(new Entity().add(title));
+
+        var info = new ImageSprite(ctx.pack.getTexture("jelly/info"));
+        info.x._ = System.stage.width/3.5;
+        info.y._ = System.stage.height/2.7;
+        scene.addChild(new Entity().add(info));
+
         var titlejelly = new ImageSprite(ctx.pack.getTexture("jelly/titlejelly"));
-        titlejelly.x._ = System.stage.width/10;
-        titlejelly.y._ = System.stage.height/3;
+        titlejelly.x._ = System.stage.width/11;
+        titlejelly.y._ = System.stage.height/2;
         titlejelly.scaleX.animate(0.25, 1, 0.5, Ease.backOut);
         titlejelly.scaleY.animate(0.25, 1, 0.5, Ease.backOut);
         scene.addChild(new Entity().add(titlejelly));
 
-        var play = new ImageSprite(ctx.pack.getTexture("buttons/base"));
-        play.centerAnchor().setXY(System.stage.width/1.4, System.stage.height/1.5);
+        var play = new ImageSprite(ctx.pack.getTexture("jelly/playbutton"));
+        play.centerAnchor().setXY(System.stage.width/1.35, System.stage.height/1.3);
         play.scaleX.animate(0.25, 1, 0.5, Ease.backOut);
         play.scaleY.animate(0.25, 1, 0.5, Ease.backOut);
         play.pointerDown.connect(function (_) {
